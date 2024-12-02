00c9abc6a8684c27a2a7f20dcf6020d8b1dea537
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:42:10 +0300

    Update book_details_view.dart

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -3,10 +3,8 @@ import 'package:booky_app/features/home/presentation/manager/similar_books_cubit
 import 'package:booky_app/features/home/presentation/views/widget/book_details_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 class BookDetailsView extends StatefulWidget {
   const BookDetailsView({super.key, required this.bookModel});
-
   final BookModel bookModel;
   @override
   State<BookDetailsView> createState() => _BookDetailsViewState();
