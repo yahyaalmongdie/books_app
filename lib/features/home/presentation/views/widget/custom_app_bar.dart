ea44f1d430591a3076ff9d0022f01f1b810712bf
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:43:44 +0300

    Update custom_app_bar.dart

--- lib/features/home/presentation/views/widget/custom_app_bar.dart
+++ lib/features/home/presentation/views/widget/custom_app_bar.dart
@@ -3,7 +3,6 @@ import 'package:booky_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
-
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
 
