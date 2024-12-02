2ffabbf98dbe182d801281d69122c38c330d4e7b
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:43:58 +0300

    Update custom_app_bar_book_details.dart

--- lib/features/home/presentation/views/widget/custom_app_bar_book_details.dart
+++ lib/features/home/presentation/views/widget/custom_app_bar_book_details.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
 class CustomAppBarBookDetails extends StatelessWidget {
   const CustomAppBarBookDetails({super.key});
 
