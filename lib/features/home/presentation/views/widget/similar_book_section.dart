c96fa5f8ea6a9f242a0dd50f21e8fb42a6c3f123
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:46:13 +0300

    Update similar_book_section.dart

--- lib/features/home/presentation/views/widget/similar_book_section.dart
+++ lib/features/home/presentation/views/widget/similar_book_section.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 class SimilarBookSection extends StatelessWidget {
   const SimilarBookSection({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Column(
