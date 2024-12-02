1e3ec83c85d6394759a18b26fe96d5442a07b9cd
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:44:14 +0300

    Update custom_book_image.dart

--- lib/features/home/presentation/views/widget/custom_book_image.dart
+++ lib/features/home/presentation/views/widget/custom_book_image.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
 class CustomBooKImage extends StatelessWidget {
   const CustomBooKImage({super.key, required this.imageUrl});
 
