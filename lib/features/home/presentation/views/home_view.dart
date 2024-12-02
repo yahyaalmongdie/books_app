adc05a24288a1761ebc3945dd9e859c7224b2cbd
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-12-02 21:41:46 +0300

    Update home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
-
   @override
   State<HomeView> createState() => _HomeViewState();
 }
-
 
 class _HomeViewState extends State<HomeView> {
   @override
